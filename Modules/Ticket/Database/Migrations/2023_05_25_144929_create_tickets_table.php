73ea6616e65e2f998e24e89312a9d8b08746ba2d
ashkan karimi <karimiashkan8186@gmail.com>
2023-05-25 19:08:23 +0330

    ****

--- Modules/Ticket/Database/Migrations/2023_05_25_144929_create_tickets_table.php
+++ Modules/Ticket/Database/Migrations/2023_05_25_144929_create_tickets_table.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Enums\EnumHelpers;
+use Modules\Ticket\Enums\EnumHelpers;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
--- Modules/Ticket/Enums/EnumHelpers.php
+++ Modules/Ticket/Enums/EnumHelpers.php
@@ -1,8 +1,6 @@
 <?php
 
-
-namespace App\Enums;
-
+namespace Modules\Ticket\Enums;
 
 class EnumHelpers
 {
